22c8b1ca521586584eda0afa495bc518f6869fe7
lmcwul <cksdnf90@gmail.com>
2024-05-02 14:27:43 +0900

    configDTO + lombok dependency 추가

--- src/main/java/com/beanions/config/BeanionsApplication.java
+++ src/main/java/com/beanions/config/BeanionsApplication.java
@@ -2,8 +2,10 @@ package com.beanions.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.beanions")
 public class BeanionsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/beanions/user/service/userServiceTemp.java
+++ src/main/java/com/beanions/user/service/userServiceTemp.java
@@ -1,5 +1,8 @@
 package com.beanions.user.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class userServiceTemp {
 
 
