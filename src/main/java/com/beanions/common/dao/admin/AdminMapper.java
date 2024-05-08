29e7c70738d1f93dfe4e36d00f5408d9356aab1b
KANGMINSEO0 <juhee0629032@gmail.com>
2024-05-08 17:03:08 +0900

    feature/adminPost commit

--- src/main/java/com/beanions/common/dao/admin/AdminMapper.java
+++ src/main/java/com/beanions/common/dao/admin/AdminMapper.java
@@ -1,6 +1,8 @@
 package com.beanions.common.dao.admin;
 
+import com.beanions.common.dto.AdminPostDTO;
 import com.beanions.common.dto.MembersDTO;
+import com.beanions.common.dto.PostDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +11,22 @@ public interface AdminMapper {
     List<MembersDTO> membersAllList();
 
     void modifyOneMember(MembersDTO modify);
+
+    List<AdminPostDTO> selectAllPost();
+
+    List<AdminPostDTO> selectPost(int code);
+
+    void postUpdate(PostDTO post);
+
+    void postDelete(int postCode);
+
+    List<AdminPostDTO> selectAllNotice();
+
+    void noticeRegist(PostDTO newNotice);
+
+    void noticeUpdate(PostDTO post);
+
+    List<AdminPostDTO> selectAllMagazine();
+
+    void magazineRegist(PostDTO newMagazine);
 }
