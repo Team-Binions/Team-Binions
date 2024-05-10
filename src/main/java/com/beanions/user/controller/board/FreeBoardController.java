355bd0cc1ac5afba7196dd518b390ae18ab1d66e
조은성 <tkakfkekdk@naver.com>
2024-05-10 12:01:16 +0900

    commit

--- src/main/java/com/beanions/user/controller/board/FreeBoardController.java
+++ src/main/java/com/beanions/user/controller/board/FreeBoardController.java
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Controller
 @RequestMapping("/user/board")
 public class FreeBoardController {
+
 
     private final FreeBoardService freeBoardService;
 
@@ -26,31 +29,39 @@ public class FreeBoardController {
     }
 
     @GetMapping("/yesinList")
-    public String yesinAllList(@ModelAttribute("params") final SearchDTO params, Model model, String id) {
+    public String yesinList(@ModelAttribute("params") final SearchDTO params, Model model, String id) {
 
-        List<PostAndMemberDTO> PostAndMemberDTOList = freeBoardService.yesinAllList(params);
+        List<PostAndMemberDTO> PostAndMemberDTOList = freeBoardService.freeList(params);
 
         model.addAttribute("PostAndMemberDTOList", PostAndMemberDTOList);
 
-        return "freeList";
+        return "/user/board/freeList";
+    }
+
+    @GetMapping("/yerangList")
+    public String yerangList(@ModelAttribute("params") final SearchDTO params, Model model, String id) {
+
+        List<PostAndMemberDTO> PostAndMemberDTOList = freeBoardService.freeList(params);
+
+        model.addAttribute("PostAndMemberDTOList", PostAndMemberDTOList);
+
+        return "/user/board/freeList";
     }
 
     @GetMapping("/yesinDetail")
     public String yesinDetail(@RequestParam("id") String id, Model model){
 
-        List<PostAndMemberDTO> PostAndMemberDTO = freeBoardService.yesinDetail(id);
+        List<PostAndMemberDTO> PostAndMemberDTO = freeBoardService.freeDetail(id);
 
         model.addAttribute("PostAndMemberDTO", PostAndMemberDTO);
 
-        return "freeDetail";
+        return "/user/board/freeDetail";
     }
 
-
-    @GetMapping("/postRegist")
+    @GetMapping("/freeRegist")
     public void postRegistPage(){}
 
-    @PostMapping("/postRegist")
-
+    @PostMapping("/freeRegist")
     public String postRegist(PostDTO postDTO, RedirectAttributes rttr, HttpSession session){
 
 //        Integer memberCode = (Integer) session.getAttribute("memberCode");
@@ -63,43 +74,56 @@ public class FreeBoardController {
 //            rttr.addFlashAttribute("errorMessage", "로그인 후에 게시글을 작성할 수 있습니다.");
 //        }
 
-        freeBoardService.postRegist(postDTO);
+        freeBoardService.freeRegist(postDTO);
 
         System.out.println("newPost = " + postDTO);
 
         rttr.addFlashAttribute("successMessage", "게시글을 등록하였습니다.");
 
+        // 예랑 게시판 생성 하면 주소 변경하기
         if (postDTO.getSubCategory().equals("예신")){
-            return "redirect:/user/board/freeList";
-        } else {
             return "redirect:/user/board/yesinList";
-        }
+        } else {
+            return "redirect:/user/board/yerangList";}
     }
 
-    @GetMapping("/modify")
+    @GetMapping("/freemodify")
     public String modifyPost(@RequestParam("id") String id, Model model){
 
-        List<PostAndMemberDTO> modify = freeBoardService.yesinDetail(id);
+        List<PostAndMemberDTO> modify = freeBoardService.freeDetail(id);
 
         model.addAttribute("modify", modify);
 
-        return "freemodify";
+        return "user/board/freemodify";
     }
 
     @PostMapping("/modify")
-    public String modifyPost(PostDTO postDTO, RedirectAttributes rttr){
+    public String yesinModify(PostDTO postDTO, RedirectAttributes rttr){
 
-        freeBoardService.modifyPost(postDTO);
+        freeBoardService.freeModify(postDTO);
 
         rttr.addFlashAttribute("successMessage", "수정 성공");
 
         // 예랑 게시판 생성 하면 주소 변경하기
         if (postDTO.getSubCategory().equals("예신")){
-            return "redirect:/user/board/freeList";
-        }else {
             return "redirect:/user/board/yesinList";
-        }
+        } else {
+            return "redirect:/user/board/yerangList";}
     }
+
+    @PostMapping("/modify")
+    public String yerangModify(PostDTO postDTO, RedirectAttributes rttr){
+
+        freeBoardService.freeModify(postDTO);
+
+        rttr.addFlashAttribute("successMessage", "수정 성공");
+
+        // 예랑 게시판 생성 하면 주소 변경하기
+        if (postDTO.getSubCategory().equals("예신")){
+            return "redirect:/user/board/yesinList";
+        } else {
+            return "redirect:/user/board/yerangList";}
+        }
 
     @PostMapping("/delete")
     public String deletePost(RedirectAttributes rttr, PostDTO postDTO){
@@ -110,10 +134,10 @@ public class FreeBoardController {
 
         // 예랑 게시판 생성 하면 주소 변경하기
         if (postDTO.getSubCategory().equals("예신")){
-            return "redirect:/user/board/freeList";
-        }else {
             return "redirect:/user/board/yesinList";
-        }
+        } else {
+            return "redirect:/user/board/yerangList";}
     }
 
 }
+
--- src/main/java/com/beanions/user/controller/board/MagazineController.java
+++ src/main/java/com/beanions/user/controller/board/MagazineController.java
@@ -1,9 +1,42 @@
 package com.beanions.user.controller.board;
 
+import com.beanions.common.dto.PostAndMemberDTO;
+import com.beanions.user.service.board.MagazineService;
+import com.beanions.user.service.board.NoticeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user/board")
 public class MagazineController {
+    private final MagazineService magazineService;
+
+    public MagazineController (MagazineService magazineService){
+        this.magazineService = magazineService;
+    }
+
+    @GetMapping("/magazine")
+    public String noticeList(Model model) {
+
+        List<PostAndMemberDTO> magazineList = magazineService.allMagazineList();
+
+        model.addAttribute("magazineList", magazineList);
+
+        return "/user/board/magazineList";
+    }
+
+    @GetMapping( "/magazinedetail")
+    public String noticeDetail(@RequestParam("id") String id, Model model) {
+
+        List<PostAndMemberDTO> megazine = magazineService.selectMagazine(id);
+
+        model.addAttribute("magazine", megazine);
+
+        return "/user/board/magazineDetail";
+    }
 }
