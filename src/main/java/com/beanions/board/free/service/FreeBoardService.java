e4868caf0fcfc2a3fcca23153234be394ddba50c
조은성 <tkakfkekdk@naver.com>
2024-05-16 19:01:33 +0900

    commit

--- src/main/java/com/beanions/board/free/service/FreeBoardService.java
+++ src/main/java/com/beanions/board/free/service/FreeBoardService.java
@@ -4,9 +4,13 @@ import com.beanions.board.free.dao.FreeBoardMapper;
 //import com.beanions.common.dto.Page;
 import com.beanions.board.common.dto.PostAndMemberDTO;
 import com.beanions.common.dto.PostDTO;
+import com.beanions.common.dto.SearchDTO;
+import com.beanions.common.paging.Pagination;
+import com.beanions.common.paging.PagingResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -18,10 +22,22 @@ public class FreeBoardService {
         this.freeBoardMapper = freeBoardMapper;
     }
 
-    public List<PostAndMemberDTO> freeList(String id) {
+    public PagingResponse<PostAndMemberDTO> freeList(String id, final SearchDTO params) {
 
-        return freeBoardMapper.freeList(id);
+        // 조건에 해당하는 데이터가 없는 경우, 응답 데이터에 비어있는 리스트와 null을 담아 반환
+        int count = freeBoardMapper.count(params);
+        if (count < 1) {
+            return new PagingResponse<>(Collections.emptyList(), null);
+        }
 
+        // Pagination 객체를 생성해서 페이지 정보 계산 후 SearchDto 타입의 객체인 params에 계산된 페이지 정보 저장
+        Pagination pagination = new Pagination(count, params);
+        params.setPagination(pagination);
+
+        // 계산된 페이지 정보의 일부(limitStart, recordSize)를 기준으로 리스트 데이터 조회 후 응답 데이터 반환
+        List<PostAndMemberDTO> list = freeBoardMapper.freeList(id, params);
+
+        return new PagingResponse<>(list, pagination);
     }
 
     public List<PostAndMemberDTO> freeDetail(String id) {
