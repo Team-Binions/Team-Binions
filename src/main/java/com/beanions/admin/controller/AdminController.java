d5ca77b601c67683de3ec42352bc0b196c23fd4f
lmcwul <cksdnf90@gmail.com>
2024-05-20 16:11:21 +0900

    [FEATURE][ADMIN] 회원 상세 조회&수정 보완
    1. 회원 조회 시 파일가져와 출력
    
    2. 개발툴 활성화시켜서 폴더 업데이트 진행해야지만 파일이 깨지지 않고 가져오는 이슈 해결
    
    3. 회원가입 유효성 이슈 보완
    
    4. yml 정적 리소스 절대 경로 추가

--- src/main/java/com/beanions/admin/controller/AdminController.java
+++ src/main/java/com/beanions/admin/controller/AdminController.java
@@ -4,15 +4,22 @@ import com.beanions.admin.dto.AdminMemberPostDTO;
 import com.beanions.common.dto.MembersDTO;
 import com.beanions.admin.service.AdminMemberService;
 import com.beanions.common.dto.PostDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin")
@@ -48,6 +55,7 @@ public class AdminController {
         List<PostDTO> memberOnePosts = adminMemberService.selectMemberPost(codes);
         AdminMemberPostDTO usersReviewDataList = adminMemberService.selectAdminReviewData(codes);
 
+        System.out.println("조회 된 회원 : " + memberDetail);
         System.out.println("codes = " + codes);
 
         model.addAttribute("memberDetail", memberDetail);
@@ -65,22 +73,58 @@ public class AdminController {
         int code = Integer.parseInt(id);
 
         MembersDTO memberModify = adminMemberService.selectMembers(code);
+        AdminMemberPostDTO userWritePostCount = adminMemberService.selectAdminReviewData(code);
 
         System.out.println("memberModify = " + memberModify);
 
         model.addAttribute("memberModify", memberModify);
+        model.addAttribute("userWritePostCount", userWritePostCount);
 
         return "/admin/member/membersModify";
     }
 
     //    @PostMapping("member/membersModify/{id}")
+
+    @PostMapping(value="/request-verify-wedd-admin")
+    @ResponseBody
+    public String requestUploadVefFile(@RequestParam(value="file",required = false) MultipartFile file) throws Exception {
+
+        /* 서버로 전달 된 File을 서버에서 설정하는 경로에 저장한다. */
+        String root = "src/main/resources/assets/images/upload";
+        String filePath = root + "/user/signupTemp";
+
+        File dir = new File(filePath);
+
+        if( !dir.exists() ) {
+            dir.mkdirs();
+        }
+
+        /* 파일명 변경하기 */
+        String originFileName = file.getOriginalFilename();
+        System.out.println("originFileName : " + originFileName);
+        String ext = originFileName.substring(originFileName.lastIndexOf("."));
+//        System.out.println("ext : " + ext);
+
+        String savedName = UUID.randomUUID() + ext;
+        System.out.println("savedName : " + savedName);
+
+        /* 파일 저장 */
+        try {
+            Path path= Paths.get(filePath + "/" + savedName).toAbsolutePath();
+            file.transferTo(path.toFile());
+//            file.transferTo(new File(filePath + "/" + savedName));
+
+        } catch (IOException e) {
+            System.out.println("error : " + e);
+        }
+
+        return new ObjectMapper().writeValueAsString(savedName);
+    }
     @PostMapping("/member/update")
-    public String memberModify(MembersDTO modify, RedirectAttributes rttr) {
+    public Object memberModify(MembersDTO modify, RedirectAttributes rttr) {
 
         System.out.println("modify = " + modify);
-
         adminMemberService.memberModify(modify);
-
 
         rttr.addFlashAttribute("successMessage", "해당 회원의 정보가 수정 되었습니다.");
 
