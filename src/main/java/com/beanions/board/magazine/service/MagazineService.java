1cee4105c62138a8e64220697d15d748c8739f48
Bibi0104 <goqlsdl0120@gmail.com>
2024-05-20 18:53:41 +0900

    매거진 css 레이아웃, board header common.js 연결

--- src/main/java/com/beanions/board/magazine/service/MagazineService.java
+++ src/main/java/com/beanions/board/magazine/service/MagazineService.java
@@ -2,9 +2,9 @@ package com.beanions.board.magazine.service;
 
 import com.beanions.board.common.dto.PostAndMemberDTO;
 import com.beanions.board.magazine.dao.MagazineMapper;
-import com.beanions.common.dto.SearchDTO;
-import com.beanions.common.paging.Pagination;
-import com.beanions.common.paging.PagingResponse;
+import com.beanions.board.magazine.dto.MagazineDTO;
+import com.beanions.board.magazine.controller.Pagination;
+import com.beanions.board.magazine.controller.PagingResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -18,7 +18,7 @@ public class MagazineService {
         this.magazineMapper = magazineMapper;
     }
 
-    public PagingResponse<PostAndMemberDTO> allMagazineList(final SearchDTO params) {
+    public PagingResponse<PostAndMemberDTO> allMagazineList(final MagazineDTO params) {
 // 조건에 해당하는 데이터가 없는 경우, 응답 데이터에 비어있는 리스트와 null을 담아 반환
         int count = magazineMapper.count(params);
         System.out.println("params = " + params);
