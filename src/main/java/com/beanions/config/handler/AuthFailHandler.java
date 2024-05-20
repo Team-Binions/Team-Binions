946b3151e3479d5401addcb866a5d7209651e8fd
경희 <kkh02046@gmail.com>
2024-05-20 20:10:21 +0900

    add modal/popup in login

--- src/main/java/com/beanions/config/handler/AuthFailHandler.java
+++ src/main/java/com/beanions/config/handler/AuthFailHandler.java
@@ -27,16 +27,16 @@ public class AuthFailHandler extends SimpleUrlAuthenticationFailureHandler {
         String errorMessage = "";
 
         if( exception instanceof BadCredentialsException ) {
-            errorMessage = "아이디가 존재하지 않거나 비밀번호가 일치하지 않습니다.";
+            errorMessage = "no-match";
         } else if ( exception instanceof InternalAuthenticationServiceException ) {
-            errorMessage = "서버에서 오류가 발생하였습니다. 관리자에게 문의해주세요.";
+            errorMessage = "error_s";
         } else if ( exception instanceof UsernameNotFoundException ) {
-            errorMessage = "존재하지 않는 ID입니다.";
+            errorMessage = "no-id";
         } else if ( exception instanceof AuthenticationCredentialsNotFoundException ) {
             // 보안 컨텍스트에 인증 객체가 존재하지 않거나, 인증 정보가 없는 생태에서 보안처리된 리소스에 접근하는 경우 발생
-            errorMessage = "인증 요청이 거부되었습니다.";
+            errorMessage = "reject";
         } else {
-            errorMessage = "알 수 없는 오류로 로그인 요청을 처리할 수 없습니다. 관리자에게 문의해주세요.";
+            errorMessage = "error_n";
         }
 
         // URL을 안전하게 인코딩
