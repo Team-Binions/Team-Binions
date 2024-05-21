a32a6f0126d437a02fcdbe6512225b388899a7fc
eunseongjo <tkakfkekdk4@gmail.com>
2024-05-21 19:34:15 +0900

    c

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 # MySQL 서버 시작
 service mysql start
-service redis start //추가
+# MySQL 초기 설정 및 사용자 호스트 변경
+mysql -u root -p${1234} <<EOF
+UPDATE mysql.user SET host='localhost' WHERE user='ohgiraffers' AND host='%';
+FLUSH PRIVILEGES;
+EOF
 # Spring Boot 애플리케이션 실행
 java -jar /app.jar
