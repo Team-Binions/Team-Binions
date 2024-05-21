fad6173fd0f7b015dcea1d1a4c97baac3207bbfb
Bibi0104 <goqlsdl0120@gmail.com>
2024-05-21 16:31:58 +0900

    500 에러

--- src/main/java/com/beanions/config/handler/GlobalExceptionHandler.java
+++ src/main/java/com/beanions/config/handler/GlobalExceptionHandler.java
@@ -1,15 +1,27 @@
 package com.beanions.config.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.io.IOException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public String exceptionHandler(Exception e) {
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoHandlerFoundException(NoHandlerFoundException ex) {
         return "error/404";
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleGeneralException(Exception ex) {
+        return "error/500";
+    }
 }
